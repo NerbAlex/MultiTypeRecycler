72a24a336b70277c3df3855b163881a3e192189a
AlexNerby <alexnerbygit@gmail.com>
2021-08-15 13:55:37 +0300

    create models interface

--- app/src/main/java/ru/inc/extendedrecycler/models/MainItemContainer.kt
+++ app/src/main/java/ru/inc/extendedrecycler/models/MainItemContainer.kt
@@ -1,3 +1,11 @@
 package ru.inc.extendedrecycler.models
 
-data class MainItemContainer(var someCard: SomeCard?, var someListItem: SomeListItem?, var titleHead: TitleHead?)
+interface MainItemContainer {
+    companion object {
+        const val TITLE_TYPE = 0
+        const val CARD_TYPE = 1
+        const val ID_TYPE = 2
+    }
+
+    fun getType(): Int
+}
